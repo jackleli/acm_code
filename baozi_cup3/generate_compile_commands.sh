8358e12241f43ff0698dd7b21bf03d2f09cc4ded
jackleli <1245099282@qq.com>
2026-03-07 15:34:06 +0800

    delete baozi_cup3

--- baozi_cup3/generate_compile_commands.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR=$(cd $(dirname $0); pwd)
-echo $DIR
-rm -f $DIR/compile_commands.json && bazel run //:refresh_compile_commands
--- baozi_cup3/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ "${1}.cpp" -o "$1" -std=c++20 && ./"$1" < input.txt > output.txt
